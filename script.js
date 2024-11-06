be62becb021226f8525663c8264265181c89b0db
Robert <robertrussell4993@gmail.com>
2024-11-06 18:30:45 +0000

    function improvements

--- script.js
+++ script.js
@@ -1,66 +1,181 @@
 console.log('hi')
 
+/*
 
+function Project (name_) {
+    let projectName = name_;
+    let tasks = [];
 
-class Project {
-    constructor(name) {
-        this.name = name;
-        this.tasks = [];
+    const AddTask = Task();
+
+    const deleteTask = (taskNumber) => {
+        tasks.splice(taskNumber, 1);
     }
 
-    addTask = (title, description, priority) => {
-        this.tasks.push(createTask(title, description, priority))
-    }
-
-    deleteTask = (taskNumber) => {
-        this.tasks.splice(taskNumber, 1);
-    }
-
-    printTasks = () => printTasks(this.tasks);
+    const printTasks = () => print_Tasks(tasks);
+  
+    return {projectName, tasks, AddTask, deleteTask, printTasks}
 }
 
-function printTasks(taskStore) {
+
+
+
+function Project(title) {
+    const projectTasks = [];
+    return {title, projectTasks }
+}
+
+
+const ProjectActions = (function () {
+    const CreateProject = (projectName) => { 
+        ProjectList.push(Project(projectName))
+    }
+
+    const deleteProject = (projectNumber) => {
+        ProjectList.splice(projectNumber, 1)
+    }
+
+    const listProjects = () => {
+        ProjectList.forEach(element => {
+            console.log(element.projectName)
+            
+        });
+    }
+
+    const AddTask = () => Task();
+
+    return {CreateProject, deleteProject, listProjects, AddTask}
+})();
+
+
+
+
+
+
+
+
+function Task(title_, description_, priority_) {
+    const title = TaskItem(title_);
+    const description = TaskItem(description_);
+    const priority = TaskItem(priority_);
+    const completed = false;
+
+    const task = () => {
+        {title, description, priority, completed};
+    }
+
+
+    const ToggleComplete = () => { 
+        completed = !task.completed;
+    }
+
+        
+    return {task, ToggleComplete}
+}
+
+
+/*
+function createTask(title_, description_, priority_) {
+    const title = TaskItem(title_);
+    const description = TaskItem(description_);
+    const priority = TaskItem(priority_);
+    const completed = false;
+    return {title, description, priority, completed}
+}
+
+
+function AddTask(project, title, description, priority) {
+    project.tasks.push(createTask(title, description, priority));
+}
+function ToggleComplete(task) {
+    task.completed = !task.completed;
+}
+
+
+
+*/
+
+
+
+
+
+function print_Tasks(taskStore) {
     taskStore.forEach(element => {
-        console.log(`Title: ${element.title.item}, Desc: ${element.description.item}, Priority: ${element.priority.item}`)
+        console.log(`Title: ${element.title.getItem()}, Desc: ${element.description.getItem()}, Priority: ${element.priority.getItem()}, Completed: ${element.completed}`)
     });
 }
 
-
-function createTask(title_, description_, priority_) {
-    const title = new TaskItem(title_);
-    const description = new TaskItem(description_);
-    const priority = new TaskItem(priority_);
-
-    return {title, description, priority}
-}
+ProjectList = []
 
 
-class TaskItem {
-    constructor(item) {
-        this.item = item;
+
+function TaskItem(item_) {
+    let item = item_;
+
+    const getItem = () => {
+        return item;
     }
 
-    getItem = () => {
-        return this.item;
+    const updateItem = (newTitle) => {
+        item = newTitle;
     }
 
-    updateItem = (newTitle) => {
-        this.item = newTitle;
+    const capataliseItem = () => {
+        item = item.toUpperCase();
     }
 
-    capataliseItem = () => {
-        this.item = this.item.toUpperCase();
-    }
+    return {getItem, updateItem, capataliseItem}
 }
 
 
 
+createProject('MyProject')
+
+createProject('2nd Project')
+
+ProjectList.projects[0].AddTask('Tasks title', 'A task desc', 'priority' )
 
 
-const Project1 = new Project('Project1');
+/*
 
-Project1.addTask('Tasks title', 'A task desc', 'priority')
-Project1.addTask('Tasks title2', 'A task desc2', 'priority2')
+
+AddTask(Projects.projects[0], 'Tasks title2', 'A task desc2', 'priority2' )
+AddTask(Projects.projects[0], 'Tasks title3', 'A task desc3', 'priority3' )
+
+
+AddTask(Projects.projects[1], 'AAAAAAA', 'A task desc', 'priority' )
+AddTask(Projects.projects[1], 'TBBBBBBBB', 'A task desc2', 'priority2' )
+AddTask(Projects.projects[1], 'TaCCCCCCCCCCC3', 'A task desc3', 'priority3' )
+
+Projects.projects[0].printTasks()
+Projects.projects[0].tasks[1].title.capataliseItem();
+Projects.projects[0].printTasks()
+
+Projects.projects[0].deleteTask(1);
+
+Projects.projects[0].printTasks()
+
+Projects.projects[1].printTasks()
+Projects.projects[1].tasks[1].title.capataliseItem();
+Projects.projects[1].printTasks()
+
+Projects.projects[1].deleteTask(2);
+
+Projects.projects[1].printTasks()
+
+
+
+
+
+const Project1 = Project('Project1');
+
+createTask(Project1,'Tasks title', 'A task desc', 'priority' )
+createTask(Project1,'Tasks title2', 'A task desc2', 'priority2' )
+
+//console.log(Project1.tasks[0].title.getItem())
+
+//Project1.addTask('Tasks title', 'A task desc', 'priority')
+//Project1.addTask('Tasks title2', 'A task desc2', 'priority2')
 
 
 Project1.printTasks();
@@ -76,3 +191,5 @@ console.log(Project1.tasks[1].title.getItem());
 //const test = new Task('Tasks title', 'A task desc', 'priority')
 
 //console.log(test.title)
+
+*/
---
+++ script2.js
@@ -0,0 +1,96 @@
+// Project object
+const ProjectObject = (name_) => {
+    const name = name_;
+    const date = '04/12/2024'
+    const tasks = [];
+
+    const addTask = (task_info) => {
+        tasks.push(Task(task_info))
+    }
+    return {name, date, tasks, addTask}
+}
+
+// Storing projects
+const Projects = (function () {
+    const projects = [];
+
+    const addProject = (projectName) => { 
+        projects.push(ProjectObject(projectName))
+    }
+
+    const deleteProject = (projectNumber) => {
+        projects.splice(projectNumber, 1)
+    }
+
+    const listProjects = () => {
+        projects.forEach(element => {
+            console.log(element.projectName)
+            
+        });
+    }
+
+
+    return {projects, addProject, deleteProject, listProjects}
+})();
+
+
+function CreateProject(projectName) {
+    Projects.addProject(projectName);
+}
+
+// Task objects
+function Task(task_info) {
+    let title = TaskObject(task_info[0]);
+    let description = TaskObject(task_info[1]);
+    let priority = TaskObject(task_info[2]);
+    let completed = false;
+
+    const ToggleComplete = () => { 
+        completed = !completed;
+    }
+
+    const PrintTask = () => {
+        return `${title.getItem()}, ${description.getItem()}, ${priority.getItem()}, ${completed}`;
+    }
+     
+    return {PrintTask, ToggleComplete}
+}
+
+// Individual task item objects
+function TaskObject(item_) {
+    let item = item_;
+
+    const getItem = () => {
+        return item;
+    }
+
+    const updateItem = (newTitle) => {
+        item = newTitle;
+    }
+
+    const capataliseItem = () => {
+        item = item.toUpperCase();
+    }
+
+    return {getItem, updateItem, capataliseItem}
+}
+
+
+
+
+CreateProject('MyProject')
+CreateProject('2nd Project')
+
+console.log(Projects.projects[0].tasks)
+
+Projects.projects[0].addTask(['Tasks title1', 'A task desc1', 'priority1'])
+Projects.projects[0].addTask(['Tasks title2', 'A task desc2', 'priority2'])
+Projects.projects[0].addTask(['Tasks title3', 'A task desc3', 'priority3'])
+
+
+console.log(Projects.projects[0].tasks[0].PrintTask())
+Projects.projects[0].tasks[0].ToggleComplete()
+console.log(Projects.projects[0].tasks[0].PrintTask())
+
+//console.log(Projects.projects[0].tasks[0].title.getItem())
+//console.log(Projects.projects[0].tasks[1].title.getItem())
